d4f17bac086828743280388a595c213cc06e4648
segundoc361 <126724727+segundoc361@users.noreply.github.com>
2025-01-29 19:58:09 +0000

    First Commit

--- src/Hello.java
+++ src/Hello.java
@@ -1,1 +1,12 @@
 // Hello World
+
+// Class
+public class Hello {
+
+    // Main
+    public static void main(String[] args) {
+
+        //cout "Hello World"
+        System.out.println("Hello World!");
+    }
+}
